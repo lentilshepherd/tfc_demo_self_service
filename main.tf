9fdea100ec09e597a32fbe65f6b1d3641c227df4
Richard Russell <rirussel@cisco.com>
2021-10-13 18:22:14 +0100

    Fix missing = in tags

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 terraform {
-  required_version = ">= 0.11.0"
+  required_version = ">= 0.12.0"
 }
 
 provider "aws" {
-  region = "${var.aws_region}"
+  region = var.aws_region
 }
 
 resource "aws_instance" "ubuntu" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
+  ami               = var.ami_id
+  instance_type     = var.instance_type
   availability_zone = "${var.aws_region}a"
 
-  tags {
-    Name = "${var.name}"
+  tags = {
+    Name = var.name
   }
 }
