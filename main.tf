cbaa8ed2c5b28c6b31cdaab4581f1e6bd7fc343d
Richard Russell <rirussel@cisco.com>
2021-11-02 12:37:48 +0000

    remove non-compliant config

--- main.tf
+++ main.tf
@@ -6,33 +6,33 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Non-compliant config - Resource created from root module
-resource "aws_instance" "bad_ubuntu" {
-  # ubuntu bionic 
-  ami               = "ami-008485ca60c91a0f3"
-  instance_type     = "t2.micro"
-  availability_zone = "eu-west-2a"
+# # Non-compliant config - Resource created from root module
+# resource "aws_instance" "bad_ubuntu" {
+#   # ubuntu bionic 
+#   ami               = "ami-008485ca60c91a0f3"
+#   instance_type     = "t2.micro"
+#   availability_zone = "eu-west-2a"
 
-  tags = {
-    Name = "non-compliant"
-  }
-}
+#   tags = {
+#     Name = "non-compliant"
+#   }
+# }
 
-# Non-compliant config - Using module from public registry
-module "bad_ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
+# # Non-compliant config - Using module from public registry
+# module "bad_ec2_instance" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+#   version = "~> 3.0"
 
-  name = "tfc-demo-public-ec2-module"
+#   name = "tfc-demo-public-ec2-module"
 
-  ami                    = "ami-02c7ed3df628d7ba2"
-  instance_type          = "t2.micro"
+#   ami                    = "ami-02c7ed3df628d7ba2"
+#   instance_type          = "t2.micro"
 
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
+#   tags = {
+#     Terraform   = "true"
+#     Environment = "dev"
+#   }
+# }
 
 # Compliant config
 module "tfc-demo-good-two-tier" {
